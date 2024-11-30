513e825213bee38a1d298b8543dbbbf160099346
Kunal Pal <kunalpal460@gmail.com>
2024-11-30 18:05:42 +0530

    Added More code

--- Anagram.cpp
+++ Anagram.cpp
@@ -8,6 +8,35 @@
 
 using namespace std;
 
+
+bool findAns(string s1 , string s2){
+
+    if(s1.size()!=s2.size()){
+        return false;
+    }
+
+    vector<int> freq(26 , 0);
+    // vector<int> freq2(26 , 0);
+    // s1 -> freq mai.. dalo 
+    // s2 ko freq2 mai... then check karo if both freq are equal then return on that basis 
+    // ans should be unique
+
+    for(int i=0; i<s1.size(); i++){
+        freq[s1[i]-'a']++;
+        freq[s2[i]-'a']--;
+    }
+
+    for(auto i: freq){
+
+        if(i!=0){
+            return false;
+        }
+    }
+
+    return true;
+
+}
+
 bool solve(string s1 , string s2){
 
     int n1 = s1.size();
@@ -39,13 +68,15 @@ int main(){
     cout <<"Enter the Second String : " <<endl;
     cin >> str2;
 
-    bool ans = solve(str1 , str2);
+    bool ans = findAns(str1 , str2);
+
+    // bool ans = solve(str1 , str2);
 
     if(ans){
-        cout << endl <<" Both the string have same characters with same frequency. So, they are anagrams. "<<endl;
+        cout << endl <<"Both the string have same characters with same frequency. So, they are anagrams. "<< endl;
     }
     else{
-        cout <<" Characters in both the strings are not same, so they are not anagrams." <<endl;
+        cout <<"Characters in both the strings are not same, so they are not anagrams." << endl;
     }
 
     return 0;
